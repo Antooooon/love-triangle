8d6600ae87fa301533363a41149365aeeb09852e
Antooooon <rockmen2016@mail.ru>
2019-02-19 19:37:25 +0300

    second

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []){
-
+module.exports = function getLoveTrianglesCount(preferences=[]){
+    let count =0;
+    preferences.forEach(function(item,i,arr){
+        if(item>0 && arr[item-1] > 0 && i+1!=arr[item-1] && i+1 === arr[arr[item-1]-1]){
+            count++;
+        }
+    })
+    return count/3;
 };
--- test.js
+++ test.js
@@ -9,7 +9,7 @@ describe('Love triangles', () => {
   });
 
   it('2', () => {
-    const count = getLoveTrianglesCount([2, 3, 1, 5, 6, 4]);
+    const count = getLoveTrianglesCount([2, 3, 1, 5, 6, 4]); 
     assert.equal(count, 2);
   });
 
